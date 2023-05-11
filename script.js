49f14d4d66de6c7e70662db7f0e4e26cde901859
Felipe Zucoli Colombari <131832858+Felipe-Zucoli@users.noreply.github.com>
2023-05-11 10:42:48 -0300

    Added Functions

---
+++ script.js
@@ -0,0 +1,15 @@
+var form = document.getElementById("contact-form");
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    var name = form.elements["nome"].value;
+    var email = form.elements["email"].value;
+    var mensagem = form.elements["mensagem"].value;
+
+    var resultado = `Nome: ${name} \nE-mail: ${email} \nMensagem: ${mensagem}`;
+    alert(resultado);
+    console.log (resultado);
+}
+
+form.addEventListener("submit", handleSubmit);
